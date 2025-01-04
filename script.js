7ee0a92513ba1e3780404210af4a8b94928b5016
Abdul Qureshi <abdulqureshi@abduls-air.home>
2025-01-04 00:36:42 -0500

    Add logic to display latest game result for leafs

--- script.js
+++ script.js
@@ -1,7 +1,51 @@
-apiUrl = "https://api-web.nhle.com/v1/scoreboard/TOR/now";
+(function() {
+    var cors_api_host = 'cors-anywhere.herokuapp.com';
+    var cors_api_url = 'https://' + cors_api_host + '/';
+    var slice = [].slice;
+    var origin = window.location.protocol + '//' + window.location.host;
+    var open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function() {
+        var args = slice.call(arguments);
+        var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+        if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+            targetOrigin[1] !== cors_api_host) {
+            args[1] = cors_api_url + args[1];
+        }
+        return open.apply(this, args);
+    };
+})();
+
+apiUrl = "https://cors-anywhere.herokuapp.com/https://api-web.nhle.com/v1/scoreboard/TOR/now";
 
 function parseResult(data) {
-    return data.games.first();
+    const today = new Date().toISOString();
+    let closestDate = null;
+    let closestDateDiff = Infinity;
+    let closestGame = null;
+
+    data.gamesByDate.forEach(gamesByDate => {
+        const gameDate = new Date(gamesByDate.games[0].startTimeUTC);
+        const diff = Math.abs(new Date(today) - gameDate);
+
+        if (gameDate <= new Date(today) && diff < closestDateDiff) {
+            closestDate = gamesByDate.date;
+            closestDateDiff = diff;
+            closestGame = gamesByDate.games[0];
+        }
+    });
+
+    if (closestGame && closestGame.gameState != "FUT") {
+        let homeTeamScore = closestGame.homeTeam.score;
+        let awayTeamScore = closestGame.awayTeam.score;
+
+        if (homeTeamScore > awayTeamScore) {
+            return `${closestGame.homeTeam.name.default} won against ${closestGame.awayTeam.name.default} on ${closestDate}`;
+        } else {
+            return `${closestGame.awayTeam.name.default} won against ${closestGame.homeTeam.name.default} on ${closestDate}`;
+        }
+    } else {
+        return 'No recent game found.';
+    }
 }
 
 fetch(apiUrl)
